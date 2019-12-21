be1133cb72068f4eec0464ce083b16269c32bff5
Mohammed Esafi <mesafi@e1r9p14.1337.ma>
2019-12-21 13:45:31 +0100

    INSTALLER v1.0.1

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@
 RESET='\033[0m'
 BOLD='\033[01m'
 GREEN='\033[32m'
+ITALIC='\033[03m'
 
 curl -o ~/.cleaner.sh https://raw.githubusercontent.com/occulte/42Cleaner/master/cleaner.sh &> /dev/null
 chmod +x ~/.cleaner.sh
@@ -26,6 +27,5 @@ BASHRC='.bash_profile' ; ZSHRC='.zshrc'
 
 Add $BASHRC ; Add $ZSHRC
 
-source $HOME/$BASHRC ; source $HOME/$ZSHRC
-
-printf "${GREEN}${BOLD}‣ please open a new shell to finish installation!${RESET}\n"
+printf "${GREEN}${BOLD}‣ installation finished successfully!${RESET}\n"
+printf "${ITALIC}  to start using it, open a new tab or \`source ~/${BASHRC}\`!${RESET}\n"
